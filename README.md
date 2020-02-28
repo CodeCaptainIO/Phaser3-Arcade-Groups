96034f391dd93264debfd54e7d34d3a93f44e9bc
Sabatino Masala <Masalasabatino@gmail.com>
2020-02-28 15:18:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Phaser 3 Arcade Physics Group example
+
+Blogpost: https://www.codecaptain.io/blog/game-development/shooting-bullets-using-phaser-groups/518
+
+This demo shows how to work with groups in PhaserJS.
+
+# Features
+
+- How to set up a Phaser Group
+- Create an Arcade physics body for movement
+- Detect when lasers are out of bound
+- Object Pooling using Phaser Groups for improving performance
+- Phaser mouse or touch input
+- Phaser keyboard input events
---
+++ static/js/main.js
@@ -0,0 +1,33 @@
+class SpaceScene extends Phaser.Scene
+{
+	constructor() {
+		super();
+	}
+
+	preload() {
+		this.load.image('laser', '/static/img/assets/laserBlue02.png');
+		this.load.image('ship', '/static/img/assets/playerShip1_red.png');
+	}
+
+	create() {
+	}
+
+	update() {
+	}
+}
+
+const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 700,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: false,
+			gravity: { y: 0 }
+		}
+	},
+	scene: SpaceScene
+};
+
+const game = new Phaser.Game(config);
