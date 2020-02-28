a87664137793a6da48dbb61dbfc2b5499d7a3b86
Sabatino Masala <Masalasabatino@gmail.com>
2020-02-28 15:27:00 +0100

    Wrap up example

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,51 @@
+class Laser extends Phaser.Physics.Arcade.Sprite
+{
+	constructor(scene, x, y) {
+		super(scene, x, y, 'laser');
+	}
+
+	fire(x, y) {
+		this.body.reset(x, y);
+
+		this.setActive(true);
+		this.setVisible(true);
+
+		this.setVelocityY(-900);
+	}
+
+}
+
+class LaserGroup extends Phaser.Physics.Arcade.Group
+{
+	constructor(scene) {
+		super(scene.physics.world, scene);
+
+		this.createMultiple({
+			frameQuantity: 30,
+			key: 'laser',
+			active: false,
+			visible: false,
+			classType: Laser
+		});
+	}
+
+	fireBullet(x, y) {
+		const laser = this.getFirstDead(false);
+
+		if(laser) {
+			laser.fire(x, y);
+		}
+	}
+}
+
 class SpaceScene extends Phaser.Scene
 {
 	constructor() {
 		super();
+
+		this.ship;
+		this.laserGroup;
+		this.inputKeys;
 	}
 
 	preload() {
@@ -12,7 +56,10 @@ class SpaceScene extends Phaser.Scene
 	create() {
 		this.cameras.main.setBackgroundColor(0x1D1923);
 
+		this.laserGroup = new LaserGroup(this);
+
 		this.addShip();
+		this.addEvents();
 	}
 
 	addShip() {
@@ -21,7 +68,37 @@ class SpaceScene extends Phaser.Scene
 		this.ship = this.add.image(centerX, bottom - 90, 'ship');
 	}
 
+	addEvents() {
+
+		// Moving the mouse should move the ship
+		this.input.on('pointermove', (pointer) => {
+			this.ship.x = pointer.x;
+		});
+
+		// Clicking the mouse should fire a bullet
+		this.input.on('pointerdown', (pointer) => {
+			this.fireBullet();
+		});
+
+		// Firing bullets should also work on enter / spacebar press
+		this.inputKeys = [
+			this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+			this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER)
+		];
+	}
+
+	fireBullet() {
+		this.laserGroup.fireBullet(this.ship.x, this.ship.y - 20);
+	}
+
 	update() {
+		// Loop over all keys
+		this.inputKeys.forEach(key => {
+			// Check if the key was just pressed, and if so -> fire the bullet
+			if(Phaser.Input.Keyboard.JustDown(key)) {
+				this.fireBullet();
+			}
+		});
 	}
 }
 
